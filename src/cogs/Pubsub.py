557a982a5bd62f66187776fa985c7eda6d6d8ad7
zenith110 <thequake@live.com>
2020-09-25 20:44:09 -0400

    Added pubsub revamp

--- src/cogs/Pubsub.py
+++ src/cogs/Pubsub.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import pubsub
+import json
+import re
 class Pubsub(commands.Cog):
     """
     sets up the basic components of the class
@@ -12,11 +14,24 @@ class Pubsub(commands.Cog):
         self.bot = bot
         self._last_member = None
     @commands.command()
-    async def getPubSub(self, context):
-        sub = pubsub.get_pub_sub()
-        pubsub_message = discord.Embed(title="Latest pubsub deal", description="Beep beep, I bring you the most current pubsub deals! \nFollow @PubSubs_on_sale")
-        pubsub_message.add_field(name="Latest pubsub deal offering", value=sub)
-        await context.send(embed=pubsub_message)
+    async def getPubSub(self, context, *, sub_name):
+        sub = pubsub.get_pub_sub(sub_name)
+        original_sub_name = sub_name.replace("-", " ")
+        sub_name_changed = sub_name.replace(" ", "-")
+        within = sub[sub_name_changed][0]
+        sub_name = within["sub_name"]
+        last_sale = within["last_sale"]
+        status = within["status"]
+        price = within["price"]
+        image = within["image"]
+        if(status == "True"):
+            pubsub_message = discord.Embed(title="Latest deal for pubsubs" , description="Beep beep, I bring you the most current pubsub deals!")
+            pubsub_message.add_field(name="Latest news on " + original_sub_name, value="Current sale last from " + last_sale + " with a price of " + price)
+            pubsub_message.set_image(url=image)
+            await context.send(embed=pubsub_message)
+        else:
+            await context.send("The sub you're looking for hasn't been on sale since " + last_sale + "!")
+
 
 
 
