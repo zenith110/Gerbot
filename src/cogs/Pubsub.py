03954d3f67c89d9df77c08e91989360e408d7df1
zenith110 <thequake@live.com>
2020-10-07 21:40:50 -0400

    Fixed the pubsub command, now works as intended and some edge cases

--- src/cogs/Pubsub.py
+++ src/cogs/Pubsub.py
@@ -13,23 +13,30 @@ class Pubsub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
+        
     @commands.command()
     async def getPubsub(self, context, *, sub_name = None):
+        """
+        Fetches a delicious pubsub
+        """
         if(sub_name != None):
-            try:
-                sub = pubsub.get_pub_sub(sub_name)
+            sub = pubsub.get_pub_sub(sub_name)
+            if(sub.status_code == "OK"):
                 original_sub_name = sub_name.replace("-", " ")
                 sub_name_changed = sub_name.replace(" ", "-")
                 if(sub.status == "True"):
                     await context.send(f"{context.author.mention}, here is your {original_sub_name}, it's on sale!")
                     pubsub_message = discord.Embed(title="Latest deal for pubsubs" , description="Beep beep, I bring you the most current pubsub deals!")
-                    pubsub_message.add_field(name="Latest news on " + original_sub_name, value="Current sale last from " + last_sale + " with a price of " + price)
+                    pubsub_message.add_field(name="Latest news on " + original_sub_name, value="Current sale last from " + sub.last_sale + " with a price of " + sub.price)
                     pubsub_message.set_image(url=sub.image)
                     await context.send(embed=pubsub_message)
                 else:
-                    await context.send("The sub you're looking for hasn't been on sale since " + sub.last_sale + "!")
-            except:
-                await context.send("Looks like " + sub_name + " isn't in the database yet, try again another time!")
+                    pubsub_message = discord.Embed(title="Latest deal for pubsubs" , description="Beep beep, I bring you the most current pubsub deals!")
+                    pubsub_message.add_field(name="Last time " + original_sub_name + " was on sale", value="Last sale was from " + sub.last_sale)
+                    pubsub_message.set_image(url=sub.image)
+                    await context.send(embed=pubsub_message)
+            elif(sub.status_code == "404"):
+                await context.send("The pubsub api does not contain that sub, please try another sub!")
         else:
             sub = pubsub.empty_sub_input()
             sub_name = sub.sub_name
@@ -45,7 +52,20 @@ class Pubsub(commands.Cog):
                 pubsub_message.set_image(url=image)
                 await context.send(embed=pubsub_message)
             else:
-                await context.send("The sub you're looking for hasn't been on sale since " + last_sale + "!")
+                pubsub_message = discord.Embed(title="Latest deal for pubsubs" , description="Beep beep, I bring you the most current pubsub deals!")
+                pubsub_message.add_field(name="Last time " + sub_name + " was on sale", value="Last sale was from " + sub.last_sale)
+                pubsub_message.set_image(url=sub.image)
+                await context.send(embed=pubsub_message)
+
+    
+    @commands.command()
+    async def subs(self, context):
+        """
+        Gets the names of all the pubsubs
+        """
+        subs = pubsub.get_all_subs()
+        await context.send(f"{context.author.mention}, here is our list of subs! \n" + subs)
+
 
 
 
--- src/pubsub.py
+++ src/pubsub.py
@@ -10,17 +10,34 @@ class randomsub():
         self.on_sale = ""
         self.price = ""
         self.image = ""
+        self.status_code = ""
+
+def get_all_subs():
+    url = "https://pubsub-api.dev/allsubs/"
+    response = requests.get(url).json()
+    sub_names = []
+    for i in range(len(response)):
+        sub_names.append(response[i]["name"])
+    subs = ", ".join(sub_names)
+    return subs
+
 def get_pub_sub(sub_name):
     sub_name = sub_name.replace(" ", "-")
     url = "https://pubsub-api.dev/subs/?name=" + sub_name
-    response = requests.get(url).json()
-    sub = randomsub()
-    sub.sub_name = response[0]["sub_name"]
-    sub.last_sale = response[0]["last_sale"]
-    sub.status = response[0]["status"]
-    sub.price = response[0]["price"]
-    sub.image = response[0]["image"]
-    return sub
+    try:
+        response = requests.get(url).json()
+        sub = randomsub()
+        sub.sub_name = response[0]["sub_name"]
+        sub.last_sale = response[0]["last_sale"]
+        sub.status = response[0]["status"]
+        sub.price = response[0]["price"]
+        sub.image = response[0]["image"]
+        sub.status_code = "OK"
+        return sub
+    except:
+        sub = randomsub()
+        sub.status_code = "404"
+        return sub
 def empty_sub_input():
     sub = randomsub()
     url = "https://pubsub-api.dev/subs/?name="
