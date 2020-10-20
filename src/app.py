27b2266518fac224b53e789e112a321c9e8039db
zenith110 <thequake@live.com>
2020-10-19 22:19:39 -0400

    Added a basic run command

--- src/app.py
+++ src/app.py
@@ -7,18 +7,33 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/update/", methods =["POST", "GET"])
 def update_data():
+    going_down = DiscordWebhook(url=discord_key, content='Gerbot going down for a bit')
+    going_down_response = going_down.execute()
+
     print("Cleaning up old instances of gerbot")
     kill_process = subprocess.Popen(["sudo", "pkill", "-f", "main.py"], stdout=subprocess.PIPE)
     killoutput = kill_process.communicate()[0]
+
     print("Been pinged, let's update!")
     print("Beginning git pull")
+
     process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
     gitpull = process.communicate()[0]
+
     print("Git pull is done, now let's run the bot and the site!")
+    up = DiscordWebhook(url=discord_key, content='Gerbot is up again!')
+    up_response = up.execute()
+
     bot = subprocess.run(["sudo", "nohup", "python3", "main.py", "&", "sudo", "nohup", "python3", "app.py"], stdout=subprocess.PIPE)
     bot_info = bot.communicate()[0]
     return "Now running Gerbot!"	
-               
+
+@app.run("/run/", methods = ["POST", "GET"])    
+def run():
+    bot = subprocess.run(["sudo", "nohup", "python3", "main.py", "&", "sudo", "nohup", "python3", "app.py"], stdout=subprocess.PIPE)
+    bot_info = bot.communicate()[0]
+    return "Gerbot is running"
+    
 @app.route("/", methods =["POST", "GET"])
 def index():
         return "Please use the routes to do commands"
