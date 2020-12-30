58c289aed59df3d350f8468c117451312a7774eb
Ryan Carnovsky <rcarnovsky@gmail.com>
2020-12-30 16:47:28 -0500

    Add regex filter, add embed to show user which classes they are in

--- src/cogs/CustomStudyRoom.py
+++ src/cogs/CustomStudyRoom.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import re
 
 class CustomStudyRoom(commands.Cog):
     """
@@ -20,24 +21,68 @@ class CustomStudyRoom(commands.Cog):
     return - nothing
     """
 
-    @commands.command(aliases=['createstudyroom', 'studyroom'])
+    @commands.command(aliases=['createstudyroom', 'studyroom', 'sr'])
     async def createStudyRoom(self, context, class_code = None):
         # Create a new voice channel for class
         if discord.utils.get(context.guild.voice_channels, name=class_code):
             await context.send(f"Study room for {class_code} already exists")
             return
         
-        try:
-            category = discord.utils.get(context.guild.categories, name="Study Rooms")
-            await context.guild.create_voice_channel(class_code, category=category)
-            await context.send(f"Created study room for {class_code}")
-        except:
-            await context.send(f"Failed")
+        if class_code is None:
+            roles = context.author.roles
+            classes = []
+
+            for role in roles:
+                r = role.name.split("-")
+                x = re.match("([a-z][a-z][a-z]\d\d\d\d)", r[0])
+                if x != None:
+                    classes.append(role.name)
+
+            class_string = ""
+
+            for c in classes:
+                class_string = class_string + f"{c}\n"
+            
+            embed = discord.Embed(title="Classes", description="To create a study room, use !studyroom <role>")
+            embed.add_field(name="You have the following classes:", value=class_string)
+
+            await context.send(embed=embed)
+        else:
+            try:
+                # custom_settings = {
+                #     context.guild.default_role: discord.PermissionOverwrite(connect=False),
+                #     context.guild.me: discord.PermissionOverwrite(connect=True),
+                # }
+                valid_roles = []
+
+                for role in context.guild.roles:
+                    r = role.name.split("-")
+                    x = re.match("([a-z][a-z][a-z]\d\d\d\d)", r[0])
+                    if x != None:
+                        valid_roles.append(role.name)
+
+                if not class_code in valid_roles:
+                    await context.send(f"{class_code} is not a valid class")
+                    return
+
+                category = discord.utils.get(context.guild.categories, name="Study Rooms")
+
+                await context.guild.create_voice_channel(class_code, category=category)
+                await context.send(f"Created study room for {class_code}")
+                chan = discord.utils.get(context.guild.voice_channels, name=class_code)
+            except:
+                await context.send(f"Failed")
         
-        return        
+        return
 
     # async def deleteChannel(self, context):
         # Delete the channel if everyone leaves
+
+    @commands.command(aliases=['detail'])
+    async def roomDetail(self, context, class_code = None):
+        chan = discord.utils.get(context.guild.voice_channels, name=class_code)
+        print(chan)
+        print(context.author.name)
 
 def setup(bot):
     bot.add_cog(CustomStudyRoom(bot))
