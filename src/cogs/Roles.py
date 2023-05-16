189b5c7c84840fc6c1df1ad8b2414809d8827cf9
Abrahan N <35606009+zenith110@users.noreply.github.com>
2023-05-16 19:50:55 -0400

    Update Roles.py

--- src/cogs/Roles.py
+++ src/cogs/Roles.py
@@ -88,6 +88,7 @@ class Roles(commands.Cog):
                     "chatterjee",
                     "cruz-neira",
                     "choi",
+                    "cheng",
                     "dechev",
                     "demara",
                     "deo",
